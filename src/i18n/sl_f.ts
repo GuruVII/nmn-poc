41bc1b81607af627b20661a44c5a446dde30fd92
Guru VII <guruvii@gmail.com>
2023-06-04 21:15:51 +0200

    "translations"

--- src/i18n/sl_f.ts
+++ src/i18n/sl_f.ts
@@ -1,3 +1,22 @@
 export const sl_f = {
+  languageSelectPopup: {
+    continue: 'Nadaljuj',
+    gdprH3: 'Shranjevanje podatkov',
+    gdprP1: 'Vse stvari so shranjene na vaši napravi in ne zapustijo strežnika.',
+    gdprP2: 'Podobno kot v preteklosti z listki se gremo na t.i. častni sistem. Z uporabo FRI strategij kvarite zabavo bolj sebi.',
+    gdprP3: 'golix go away, no bulli pls'
+  },
+  popups: {
+    close: 'Zapri',
+    open: 'Odpri',
+    hintCard: {
+      follow: 'obišči me!',
+    },
+    hintCardList: {
+      title: 'Zbrane kartice',
+      follow: 'Obišči'
+    }
+  }
 
 };
+
